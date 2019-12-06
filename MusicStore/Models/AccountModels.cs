690c7ec109c6e976c537e3dc1df0d30d37ed6e4c
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-12-06 16:37:06 +0100

    AccountModel displaynema törlés

--- MusicStore/Models/AccountModels.cs
+++ MusicStore/Models/AccountModels.cs
@@ -10,18 +10,15 @@ namespace MusicStore.Models
     public class AccountModel
     {
         [Required]
-        [Display(Name = "User name")]
         public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.EmailAddress)]
-        [Display(Name = "Email address")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get; set; }
     }
     public class ChangePasswordModel
